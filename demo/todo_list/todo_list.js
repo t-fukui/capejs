694eed1436e599fd29f656c5343b2eaf0ee9ea5d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-04-07 22:27:54 +0900

    Todo List Demo: Adding items

--- demo/todo_list/todo_list.js
+++ demo/todo_list/todo_list.js
@@ -11,7 +11,8 @@
           self.items.forEach(function(item) {
             self.renderItem(b, item);
           })
-        })
+        });
+        self.renderForm(b);
       })
     },
 
@@ -26,6 +27,17 @@
       })
     },
 
+    renderForm: function(b) {
+      var self = this;
+      b.form({}, function(b) {
+        b.textField('title', { onkeyup: function(e) { self.refresh() } });
+        b.button("Add", {
+          disabled: self.getValue('title') === '',
+          onclick: function(e) { self.addItem() }
+        });
+      });
+    },
+
     init: function() {
       this.items = [
         { title: 'Foo', done: false },
@@ -36,10 +48,15 @@
     toggle: function(item) {
       item.done = !item.done;
       this.refresh();
+    },
+
+    addItem: function() {
+      this.items.push({ title: this.getValue('title'), done: false });
+      this.setValue('title', '');
+      this.refresh();
     }
   });
 
   var todo = new TodoList();
   todo.mount('todo-list');
 })();
-
