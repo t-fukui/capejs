4ad485cd88bac1835a740819021e611db6a5cb6b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-04-07 22:07:23 +0900

    Todo List Demo: Listing items

--- demo/todo_list/todo_list.js
+++ demo/todo_list/todo_list.js
@@ -7,10 +7,37 @@
     render: function() {
       var self = this;
       return this.markup(function(b) {
-        b.p('Hello World!');
+        b.ul(function(b) {
+          self.items.forEach(function(item) {
+            self.renderItem(b, item);
+          })
+        })
       })
+    },
+
+    renderItem: function(b, item) {
+      var self = this;
+      b.li(function(b) {
+        b.label({ class: { completed: item.done }}, function(b) {
+          b.input({ type: 'checkbox', checked: item.done,
+            onclick: function(e) { self.toggle(item) } });
+          b.space().text(item.title);
+        })
+      })
+    },
+
+    init: function() {
+      this.items = [
+        { title: 'Foo', done: false },
+        { title: 'Bar', done: true }
+      ];
+    },
+
+    toggle: function(item) {
+      item.done = !item.done;
+      this.refresh();
     }
-  })
+  });
 
   var todo = new TodoList();
   todo.mount('todo-list');
